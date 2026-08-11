8e953e6c7e5fd4adeb29c66cc7d8445f57025be4
Danilo Alanis <danilo.alanis@autozone.com>
2026-08-11 09:52:06 -0600

    Solved some bugs with timezone in http_data

--- projects/http_requests/src/http_requests/defs/schedules/banxico/get_current_exchange.py
+++ projects/http_requests/src/http_requests/defs/schedules/banxico/get_current_exchange.py
@@ -3,5 +3,6 @@ import dagster as dg
 exchanges_daily_schedule = dg.ScheduleDefinition(
     name="exchanges_daily_schedule",
     job_name="get_current_exchange_job",
-    cron_schedule="0 5 * * *"
+    cron_schedule="0 5 * * *",
+    execution_timezone="America/Mexico_City"
 )
